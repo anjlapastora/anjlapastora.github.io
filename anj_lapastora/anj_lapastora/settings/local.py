4e447be3a232f4086cf15e9c5c4a1ae258090d3b
Anj Lapastora <angelicalapastora@gmail.com>
2026-05-08 19:43:06 +0800

    config: Add local settings for development environment
    Introduces `local.py` to house environment-specific configurations.
    
    This includes enabling debug mode, setting up a local SQLite database, and configuring file system storage for media and static assets. It also provides a default development secret key.
    
    This streamlines local development setup and ensures development-only settings are isolated from production.

---
+++ anj_lapastora/anj_lapastora/settings/local.py
@@ -0,0 +1,51 @@
+import os
+from .base import *
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+print("🔥 USING LOCAL SETTINGS 🔥")
+
+# -------------------------
+# Core
+# -------------------------
+DEBUG = True
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
+
+ALLOWED_HOSTS = ["*"]
+
+# -------------------------
+# Database
+# -------------------------
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+# -------------------------
+# Wagtail
+# -------------------------
+WAGTAILADMIN_BASE_URL = "http://127.0.0.1:8000"
+
+# -------------------------
+# Supabase Storage (S3)
+# -------------------------
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
+
+
+# -------------------------
+# Django 4+ storage config
+# -------------------------
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
